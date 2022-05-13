5c1158036d8d941071afdf121cf88ed82325e137
Pedro Coelho <p.coelho7477@gmail.com>
2022-05-13 23:36:50 +0100

    God Class - EppsteinShortestPathIterator

--- jgrapht-master/jgrapht-core/src/main/java/org/jgrapht/alg/shortestpath/EppsteinShortestPathIterator.java
+++ jgrapht-master/jgrapht-core/src/main/java/org/jgrapht/alg/shortestpath/EppsteinShortestPathIterator.java
@@ -72,7 +72,8 @@ public class EppsteinShortestPathIterator<V, E>
     implements
     Iterator<GraphPath<V, E>>
 {
-    /**
+    private EppsteinShortestPathIteratorProduct eppsteinShortestPathIteratorProduct = new EppsteinShortestPathIteratorProduct();
+	/**
      * Underlying graph.
      */
     private final Graph<V, E> graph;
@@ -433,7 +434,7 @@ public class EppsteinShortestPathIterator<V, E>
         PathsGraphVertex restHeapRoot = null;
         int size = restHeapElements.size();
         if (size > 0) {
-            heapify(restHeapElements, size);
+        	eppsteinShortestPathIteratorProduct.heapify(restHeapElements, size);
             restHeapRoot = getRestHeap(restHeapElements, 0, size);
         }
 
@@ -441,47 +442,6 @@ public class EppsteinShortestPathIterator<V, E>
             return new Pair<>(null, restHeapRoot);
         } else {
             return new Pair<>(outroot, restHeapRoot);
-        }
-    }
-
-    /**
-     * Builds a min-heap out of the {@code vertices} list
-     *
-     * @param vertices vertices
-     * @param size size of vertices
-     */
-    private void heapify(List<PathsGraphVertex> vertices, int size)
-    {
-        for (int i = size / 2 - 1; i >= 0; i--) {
-            siftDown(vertices, i, size);
-        }
-    }
-
-    private void siftDown(List<PathsGraphVertex> vertices, int i, int size)
-    {
-        int left;
-        int right;
-        int smaller;
-        int current = i;
-
-        while (true) {
-            left = 2 * current + 1;
-            right = 2 * current + 2;
-            smaller = current;
-
-            if (left < size && vertices.get(left).compareTo(vertices.get(smaller)) < 0) {
-                smaller = left;
-            }
-
-            if (right < size && vertices.get(right).compareTo(vertices.get(smaller)) < 0) {
-                smaller = right;
-            }
-
-            if (smaller == current) {
-                break;
-            }
-            swap(vertices, current, smaller);
-            current = smaller;
         }
     }
 
@@ -505,15 +465,6 @@ public class EppsteinShortestPathIterator<V, E>
             vertices.get(i).right = getRestHeap(vertices, r, size);
         }
         return vertices.get(i);
-    }
-
-    private void swap(List<PathsGraphVertex> vertices, int i, int j)
-    {
-        if (i != j) {
-            PathsGraphVertex tmp = vertices.get(i);
-            vertices.set(i, vertices.get(j));
-            vertices.set(j, tmp);
-        }
     }
 
     /**
@@ -699,7 +650,7 @@ public class EppsteinShortestPathIterator<V, E>
      * {@code right}, {@code rest} and {@code cross} vertices, because they are computed during the
      * paths graph traversal.
      */
-    private class PathsGraphVertex
+    class PathsGraphVertex
         implements
         Comparable<PathsGraphVertex>
     {
---
+++ jgrapht-master/jgrapht-core/src/main/java/org/jgrapht/alg/shortestpath/EppsteinShortestPathIteratorProduct.java
@@ -0,0 +1,49 @@
+package org.jgrapht.alg.shortestpath;
+
+
+import java.util.List;
+import org.jgrapht.alg.shortestpath.EppsteinShortestPathIterator.PathsGraphVertex;
+
+public class EppsteinShortestPathIteratorProduct<V, E> {
+	/**
+	* Builds a min-heap out of the  {@code  vertices}  list
+	* @param vertices  vertices
+	* @param size  size of vertices
+	*/
+	public void heapify(List<PathsGraphVertex> vertices, int size) {
+		for (int i = size / 2 - 1; i >= 0; i--) {
+			siftDown(vertices, i, size);
+		}
+	}
+
+	public void siftDown(List<PathsGraphVertex> vertices, int i, int size) {
+		int left;
+		int right;
+		int smaller;
+		int current = i;
+		while (true) {
+			left = 2 * current + 1;
+			right = 2 * current + 2;
+			smaller = current;
+			if (left < size && vertices.get(left).compareTo(vertices.get(smaller)) < 0) {
+				smaller = left;
+			}
+			if (right < size && vertices.get(right).compareTo(vertices.get(smaller)) < 0) {
+				smaller = right;
+			}
+			if (smaller == current) {
+				break;
+			}
+			swap(vertices, current, smaller);
+			current = smaller;
+		}
+	}
+
+	public void swap(List<PathsGraphVertex> vertices, int i, int j) {
+		if (i != j) {
+			PathsGraphVertex tmp = vertices.get(i);
+			vertices.set(i, vertices.get(j));
+			vertices.set(j, tmp);
+		}
+	}
+}
